3fba9f77bc1e155bc19c88860d7d7e587fb143fc
khumodiatla <khumomoatlhodi@gmail.com>
2022-02-20 20:03:35 +0200

    Fixed a a bug, removed unused hook

--- src/useFetch.js
+++ src/useFetch.js
@@ -22,7 +22,7 @@ const useFetch = (url) => {
                  setError(null);
              })
              .catch( err =>{
-                 if( err.name != "AbortError"){
+                 if( err.name !== "AbortError"){
                     setIsPending(false);
                     setData(null);
                     setError(err.message);
